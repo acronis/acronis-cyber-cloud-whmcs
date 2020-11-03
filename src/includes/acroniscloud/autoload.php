03679e25d53c5775a397443a2f8fa74488a46920
Seth McCauley <9158822+JOEsDC-Seth@users.noreply.github.com>
2020-11-03 16:03:14 -0600

    Use constant for WHMCS path instead of relative path
    Use the existing ACRONIS_CLOUD_WHMCS_DIR constant to find autoload.php. This will help facilitate moving the includes directory out of webroot for security reasons.

--- src/includes/acroniscloud/autoload.php
+++ src/includes/acroniscloud/autoload.php
@@ -4,7 +4,7 @@
  */
 
 /** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__ . '/../../vendor/autoload.php';
+$loader = require ACRONIS_CLOUD_WHMCS_DIR . '/vendor/autoload.php';
 $loader->addPsr4('AcronisCloud\\', __DIR__ . '/lib');
 $loader->addPsr4('Acronis\\Cloud\\Client\\', __DIR__ . '/vendor/acronis/ci-lib-php-cloud-client/lib');
 $loader->addPsr4('Acronis\\UsageReport\\', __DIR__ . '/vendor/acronis/ci-lib-php-usage-report/src');
